38fa596c0d19e46dd3d5df346ffb09aecee8d177
leeyoonseo <okayoon.lee@gmail.com>
2020-06-06 18:08:02 +0900

    1. 한달에 몇일이 있는지 구하는 함수 생성 - 달을 기준으로 계산하는 것을 기준으로 잡을 것 2. 컨트롤하는 메인 함수 생성 3. TODO 리스트 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,12 @@
 (function(){
     
+/**
+ * [TODO] 
+ * 1. 월을 기준으로 계산하자
+ * 2. 숫자만 입력받기
+ *   
+ */
+
     let input = document.querySelectorAll('input');
     let submitBtn = document.querySelector('#submitBtn');
     let selectAge = document.querySelector('#selectAge');
@@ -8,21 +15,10 @@
     let inputWeight = document.querySelector('#inputWeight');
     let viewer = document.querySelector('#viewer');
   
-    let drawResult = ({ lowest, highest, frequency }) => {
-        // 한달 이하
-        viewer.innerHTML = '';
-
-        if(!lowest || !highest || !frequency){
-            viewer.append('모유기')
-            console.log(lowest, highest, frequency)
-
-        }else{
-            console.log(lowest, highest, frequency)
-            // 한달 이상
-        }
-    };
+    
 
     /*
+        info
         강아지
         새끼강아지 2시간에 한번 총 12번 105ml의 끓인물에 30g 파우더 스스로 그만먹을때까지
         생후 ~2주(14일)= 모유, 상황이 여의치 않는다면 새끼강아지용 우유 급여 6~8회에 걸쳐 
@@ -33,40 +29,65 @@
         12개월~5살 2~3% 하루에 2회 나누어 급여
     */
 
-    let calculator = () => {
-        let age = Number(inputAge.value);
-        let weight = Number(inputWeight.value);
-        let result = {};
+   // 일수 구하기
+   // [TODO] 일수말고, 월수를 기준으로 잡자!
+   let getDaysByMonth = (age) => {
 
+    console.log(age)
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let result = 0;
+
+        for(let i = 0; i < age; i++){
+            result = result + new Date(year, month - [i], 0).getDate()
+        }
+        
+        return result         
+   }
+
+    let weightConverter = (weight) => {
         if(selectWeight.value === 'kg'){
-            weight = weight * 1000;
+            return weight = weight * 1000;
         }
 
-        if(selectAge.value === 'year'){
-            // 1살 === 12개월
-            age = 12 / age;
-
-            drawResult({ 
-                lowest : weight * 0.2, 
-                highest : weight * 0.3,
-                frequency : 2
-            });
-
-
-        }else if(selectAge.value === 'day'){
-            // 23일 === 23일
-            if(age < 29){
-                drawResult();
-
-            // 31일 === 1.
-            }else{
-                age = age / 30;
-            }
-        }
-
-        console.log(age)
+        return weight;
     };
 
+    let ageConverter = (age) => {
+        if(selectAge.value === 'year'){
+            age = (age * 365);
+
+        }else if(selectAge.value === 'month'){
+            age = getDaysByMonth(age);
+        }
+
+        return age;
+    };
+
+    let feedCalculator = (weight, age) =>{
+        // console.log(weight, age)
+    };
+
+    let controller = () => {
+        let age = Number(inputAge.value);
+        let weight = Number(inputWeight.value);
+        weight = weightConverter(weight);     
+        age = ageConverter(age);
+
+        feedCalculator(weight, age);
+
+
+        // TODO 객체로 인자 넘기는 방법
+        // lowest : weight * 0.2, 
+        // highest : weight * 0.3,
+        // frequency : 2
+        // 그리는 함수 필요
+        // drawResult( weight * 0.2, weight * 0.3, 2 );
+    };
+
+
+    // [TODO] validation과 err에 대해 생각해보기
     let error = {
         show : (target) => {
             target.style.border = '1px solid red';
@@ -94,6 +115,7 @@
         }
     };
 
+    // [TODO] 처음에 포커싱 시키고 그 다음엔 확인 클릭마다 포커싱
     inputAge.addEventListener('focus', error.hide);
     inputWeight.addEventListener('focus', error.hide);
 
@@ -109,7 +131,7 @@
         }
 
         if(flag){
-            calculator();
+            controller();
         }
     });
 })();
