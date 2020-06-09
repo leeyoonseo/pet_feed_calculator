55c6e0b5dbc59b07159b1ad56aae8954a9ca5041
이윤서 <yoons.lee2019@gmail.com>
2020-06-09 13:34:23 +0900

    계산 로직 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -15,17 +15,7 @@
     let inputWeight = document.querySelector('#inputWeight');
     let viewer = document.querySelector('#viewer');
 
-    /*
-        info
-        강아지
-        새끼강아지 2시간에 한번 총 12번 105ml의 끓인물에 30g 파우더 스스로 그만먹을때까지
-        생후 ~2주(14일)= 모유, 상황이 여의치 않는다면 새끼강아지용 우유 급여 6~8회에 걸쳐 
-
-        2~3개월 이하 6~7% 3회 나누어 급여
-        3~6개월 몸무게의 5~6% 가량을 하루에 2~3회 나누어 급여
-        6~12 개월 4~5% 가량을 하루에 2회 나누어 급여
-        12개월~5살 2~3% 하루에 2회 나누어 급여
-    */
+ 
 
    // 일수 구하기
    // [TODO] 일수말고, 월수를 기준으로 잡자!
@@ -77,40 +67,77 @@
         return age;
     };
 
+    let draw = (num) => {
+        console.log(num)
+    }
+
+     /*
+        info
+        강아지
+        새끼강아지 2시간에 한번 총 12번 105ml의 끓인물에 30g 파우더 스스로 그만먹을때까지
+        생후 ~2주(14일)= 모유, 상황이 여의치 않는다면 새끼강아지용 우유 급여 6~8회에 걸쳐 
+
+        2~3개월 이하 6~7% 3,4회 나누어 급여
+        3~6개월 몸무게의 4~5% 가량을 하루에 2~3회 나누어 급여
+        6~12 개월 2~3% 가량을 하루에 2회 나누어 급여
+        12개월~5살 2% 하루에 2회 나누어 급여
+    */
+
     let feedCalculator = (weight, age) =>{
         console.log(weight, age);
-
+        let result = {};
+        
         // 한달 미만
         if(age <= 1){
             console.log('모유기이네요')
+
         }else if(age >= 2 && age <= 3){
             console.log('2~3개월')
+            result.lowest = weight * 0.06;
+            result.highest = weight * 0.07;
+            result.num = '3, 4';
 
-        }else if(age >= 4 && age <= 5){
-            console.log('4~5개월')
+        }else if(age >= 4 && age <= 6){
+            result.lowest = weight * 0.04;
+            result.highest = weight * 0.05;
+            result.num = '2, 3';
 
-        }else if(age >= 6 && age <= 11){
+        }else if(age >= 7 && age <= 11){
             console.log('6~11개월')
+            result.lowest = weight * 0.02;
+            result.highest = weight * 0.03;
+            result.num = '2';
+
         }else{
             console.log('1살보다많아요')
+            result.lowest = weight * 0.02;
+            result.num = '2';
+        }
+
+        return result;
+    };
+
+    let drawViewer = (info) => {
+        if(!info){
+            console.log('모유기?')
+
+        }else{
+            console.log(info)
+
+
         }
     };
 
     let controller = () => {
         let age = Number(inputAge.value);
         let weight = Number(inputWeight.value);
+        let result;
+
+        // TODO 객체로 인자 넘기는 방법
         weight = weightConverter(weight);     
         age = ageConverter(age);
 
-        feedCalculator(weight, age);
-
-
-        // TODO 객체로 인자 넘기는 방법
-        // lowest : weight * 0.2, 
-        // highest : weight * 0.3,
-        // frequency : 2
-        // 그리는 함수 필요
-        // drawResult( weight * 0.2, weight * 0.3, 2 );
+        drawViewer(feedCalculator(weight, age));
     };
 
 
